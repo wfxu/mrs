f78eb450e3cd9427a9fa91e4687a0b2f9d38a8a9
wfxu <wenfenxu@qq.com>
2020-08-27 11:00:58 +0800

    first end it

--- web/run.py
+++ web/run.py
@@ -62,13 +62,38 @@ def getMovies(user):
 
 @app.route('/movie/', methods=['GET', 'POST'])
 def movie():
-    # 1.如果没有用户登录则按照电影类别进行召回然后随机选取10个
-    # 2.如果有用户登录则按照找回表随机抽取10个
     if 'user' in session:
         movies_age, movies_gender = getMovies(session['user'])
     else:
         movies_age, movies_gender = getMovies(random.randint(1, 943))
     return render_template('movie.html', movies_age=movies_age, movies_gender=movies_gender)
+
+
+def getMoviesRecall(num):
+    sql1 = """
+        select user_id, rating
+        from tb_data a
+        where item_id = {num}
+    """
+    sql2 = """
+        select a.user_id, a.item_id, b.movie_title, a.rating
+        from tb_data a
+            left join tb_item b on a.item_id = b.movie_id
+        where a.item_id <> {num}
+    """
+    data1 = pd.read_sql(sql1.format(num=num), ENGINE)
+    data2 = pd.read_sql(sql2.format(num=num), ENGINE)
+    data1 = data1[data1.rating == data1.rating.max()]
+    data2 = pd.merge(data1, data2, how='inner', on='user_id', suffixes=('_', ''))
+    data2 = data2[(data2.rating == data2.rating.max()) & data2.item_id.isin(image_list)]
+    if data2.shape[0] <= 5:
+        data3 = data2.copy()
+    else:
+        data3 = data2.sample(5).copy()
+    movies = {}
+    for i in data3.index:
+        movies[data3.loc[i, 'item_id']] = data3.loc[i, 'movie_title']
+    return movies
 
 
 @app.route('/movie/<int:num>', methods=['GET', 'POST'])
@@ -92,16 +117,17 @@ def movieDetail(num):
         if data.loc[0, col] == 1:
             label.append(col)
     detail = [num, title, release_date, '；'.join(label), cnt, avg_rating, max_rating, min_rating]
-    movies = {'1':'one', '2':'two', '3':'three', '4':'four', '5':'five'}
+    movies = getMoviesRecall(num)
     return render_template('movie_detail.html', detail=detail, movies=movies)
 
 
-def getUserMovies(user):
-    recall = pd.read_sql("select recall from user_recall where user_id = {user}".format(user=user), ENGINE).recall[0]
+def getUserMovies(recall):
     movie_list = recall.split(',')
-    sql = "select movie_tittle as title from tb_item where movie_id = {m}"
+    movie_list = [im for im in movie_list if int(im) in image_list]
+    random.shuffle(movie_list)
+    sql = "select movie_title as title from tb_item where movie_id = {m}"
     movies = {}
-    for m in movie_list:
+    for m in movie_list[:5]:
         title = pd.read_sql(sql.format(m=m), ENGINE).title[0]
         movies[m] = title
     return movies
@@ -109,38 +135,73 @@ def getUserMovies(user):
 
 @app.route('/user/', methods=['GET', 'POST'])
 def user():
-	if 'user' in session:
-		return render_template('user.html', user=session['user'])
-	else:
-		return redirect(url_for('login'))
+    sql1 = """
+        select a.age, a.gender, a.occupation, a.zip_code, b.recall
+        from tb_user a
+            left join user_recall b on a.user_id = b.user_id
+        where a.user_id = {user}
+    """
+    sql2 = """
+        select * 
+        from tb_data a
+            left join tb_item b on a.item_id = b.movie_id
+        where user_id = {user} 
+        order by rating desc
+    """
+    if 'user' in session:
+        user = session['user']
+        data = pd.read_sql(sql1.format(user=user), ENGINE)
+        age, gender, occupation, zip_code, recall = data.loc[0, ['age', 'gender', 'occupation', 'zip_code', 'recall']]
+        data1 = pd.read_sql(sql2.format(user=user), ENGINE)
+        cnt = data1.shape[0]
+        love_movies = {}
+        licensed = data1[(data1.rating == data1.rating.max()) & data1.item_id.isin(image_list)].shape[0]
+        if licensed >= 5:
+            for i in data1[(data1.rating == data1.rating.max()) & data1.item_id.isin(image_list)].sample(5).index:
+                love_movies[data1.loc[i, 'item_id']] = data1.loc[i, 'movie_title']
+        else:
+            for i in data1[(data1.rating == data1.rating.max()) & data1.item_id.isin(image_list)].sample(licensed).index:
+                love_movies[data1.loc[i, 'item_id']] = data1.loc[i, 'movie_title']
+        classification = ['Action', 'Adventure', 'Animation', 'Children', 
+                           'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 
+                           'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 
+                           'War', 'Western']
+        love_label_dict = {}
+        for col in classification:
+            love_label_dict[col] = data1.loc[data1.rating >= 3, col].sum()
+        love_label = sorted(love_label_dict.items(), key = lambda x: x[1], reverse=True)
+        love_label = '；'.join([col[0] for col in love_label[:5]])
+        users = {'user':user, 'age':age, 'gender':gender, 'occupation':occupation, 
+                 'zip_code':zip_code, 'cnt':cnt, 'love_label':love_label}
+        movies = getUserMovies(recall)
+        return render_template('user.html', users=users, love_movies=love_movies, movies=movies)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		user = request.form['user']
-		session['user'] = user
-		db = connectDB().cursor()
-		result = db.execute("select 1 from users where name=?", (user,))
-		r = result.fetchall()
-		db.close()
-		if len(r) == 0:
-			return render_template('login.html', prompt="用户不存在，请重新登录！")
-		else:
-			return redirect(url_for('user'))
-	else:
-		return render_template('login.html', prompt="用户未登录，请登录！")
+    if request.method == 'POST':
+        user = request.form['user']
+        try:
+            int(user)
+        except ValueError:
+            return render_template('login.html', prompt="输入用户不合法！</br>您可以输入1-943的任意数字")
+        result = pd.read_sql("select 1 from tb_user where user_id = {user}".format(user=user), ENGINE)
+        if result.shape[0] == 0:
+            return render_template('login.html', prompt="用户不存在，请重新登录！</br>您可以输入1-943的任意数字")
+        else:
+            session['user'] = user
+            return redirect(url_for('user'))
+    else:
+        return render_template('login.html', prompt="请登录！</br>您可以输入1-943的任意数字")
 
 
 @app.route('/logout/')
 def logout():
-	session.pop('user', None)
-	return render_template('login.html', prompt="已注销，请登录其他账号！")
-
-
-def connectDB():
-	return sqlite3.connect(DATABASE)
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=80, debug=True)
+    app.run(host='127.0.0.1', port=80, debug=True)
